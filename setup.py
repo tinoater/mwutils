d15ae7bd37e923d22b02cb323d886ea8ffd39056
bobby <bobby@blurgroup.com>
2016-11-01 16:33:56 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mwutils',
+    version='1',
+    packages=[''],
+    url='https://github.com/tinoater/mwutils.git',
+    license='',
+    author='bobby',
+    author_email='',
+    description=''
+)
