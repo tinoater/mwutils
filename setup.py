7374a81671f316dc693c475c0a43af9ce6cccbd9
ISAAC\Martin <martinleewatts@gmail.com>
2017-02-26 10:42:25 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='mwutils',
     version='1.0.10',
-    packages=[''],
     url='https://github.com/tinoater/mwutils.git',
     license='',
     author='bobby',
     author_email='',
-    description=''
+    description='',
+    packages=find_packages(exclude=('Tests', 'Files'))
 )
