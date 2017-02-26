3dae60d742c64920d4067ed3b0862c6b9dfa040d
ISAAC\Martin <martinleewatts@gmail.com>
2017-02-26 11:42:55 +0000

    Added chromedriver files to install

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import find_packages
 
 setup(
     name='mwutils',
-    version='1.0.10',
+    version='1.0.11',
     url='https://github.com/tinoater/mwutils.git',
     license='',
     author='bobby',
     author_email='',
     description='',
-    packages=find_packages(exclude=('Tests', 'Files'))
+    packages=find_packages(exclude=('Tests', 'Files')),
+    include_package_data=True
 )
